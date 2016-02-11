a71769827a4c4a4604621f3ae0fefbcabe058833
pierrat lucas <lpierra3@info-101-18.iut-bm.univ-fcomte.fr>
2016-02-11 14:33:52 +0100

    Bit it OK

--- Bit it/ToBits.java
+++ Bit it/ToBits.java
@@ -18,28 +18,96 @@ class Value {
     byte[] bits;
     
     public Value(int value) {
-	bits = new byte[32];
-	for(int i=0;i<32;i++) bits[i] = 0;
-	convertToBits(value);
+	     bits = new byte[32];
+
+	     for(int i=0;i<32;i++) 
+          bits[i] = 0;
+
+	     convertToBits(value);
+
+       msb();
+       nbOnes();
+       
     }
 
-    public int msb() {
+    public void msb() {
+      int i=0;
+      while(i<32){
+        if (bits[i]==1){
+          System.out.print(32-i-1);
+          return;
+        }
+        i++;
+      }
+
     }
 
-    public int nbOnes() {
+    public void nbOnes() {
+     int  somme=0;
+      for (int i = 0; i<32; i++){
+        if(bits[i]==1){
+          somme++;
+        }
+      }
+
+      System.out.println(" " + somme);
     }
+
 
     private void convertToBits(int value) {	
+
+      for(int i = 0; i < 32; i++) {
+             
+            if(value - Math.pow(2,31-i) > 0) {
+                 
+                bits[i] = 1;
+                value -= Math.pow(2,31-i);
+                 
+            }
+             
+            else if(value - Math.pow(2,31-i) == 0) {
+                 
+                bits[i] = 1;
+                break;
+                 
+            }
+            // //Cette condition n'est pas obligatoire, le tableau de byte est rempli de 0 à la base.
+            // else {
+                 
+            //     bits[i] = 0;
+                 
+            // }
+             
+        }
+
     }
-}
+  }
 
     
 class ToBits {    
 
-    public static void main(String[] args) {
-	Locale.setDefault(Locale.ENGLISH);
+    public static void main(String[] args) 
+    {
+	     Locale.setDefault(Locale.ENGLISH);
 	
-	Scanner scan = new Scanner(System.in);
+	     Scanner scan = new Scanner(System.in);
+
+       int nB = scan.nextInt();
+
+       System.out.println(nB);
+
+        int a;
+
+       for (int i = 0; i< nB; i++){
+
+          a = scan.nextInt();
+
+          if (a>=1 && a<=(Math.pow(2, 32)-1)) {
+
+            Value value = new Value(a);
+
+          }
+       }
 
 	/* A COMPLETER
 	   - lire le nombre de valeurs à traiter
@@ -49,4 +117,4 @@ class ToBits {
 	 */
 	}
     }
-}
+
